b552fe701845c21cb68e42fcfbe8f4152d20ab96
Max <montrealist@users.noreply.github.com>
2020-06-22 23:48:59 -0400

    forgotten commits

--- lib/store.rb
+++ lib/store.rb
@@ -2,4 +2,11 @@ class Store < ActiveRecord::Base
     has_many :employees
     validates :name, length: { minimum: 3 }
     validates :annual_revenue, numericality: { only_integer: true, greater_than: 0 } 
+    validate :must_carry_apparel
+end
+
+def must_carry_apparel
+    if true
+        errors[:base] << "I just feel like failing"
+    end
 end
